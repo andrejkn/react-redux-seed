37ba3b121cfa167a552e384a2f1170f2e0c560bb
Uma Chandran <codebyuma@users.noreply.github.com>
2016-05-17 12:19:53 -0400

    Feat - Add http-based authentication using passport on backend (#172)
    - Update backend server files to use passport for login
    - Update frontend to use fetch and post to the backend for the login
    - Add concurrently and set up a proxy so both the node and webpack-dev servers can run at the same time.
    - Updated to run node server in strict mode to deal with conflict between eslint rules and node's support for let
    - Resolved merge conflicts and removed Express sessions

--- src/actions/session.js
+++ src/actions/session.js
@@ -1,4 +1,4 @@
-import { login } from '../api/login';
+import { login } from '../api/auth/';
 
 import {
   LOGIN_USER_PENDING,
@@ -10,8 +10,10 @@ import {
 
 export function loginUser() {
   return (dispatch, getState) => {
-    const username = getState().form.login.username.value;
-    const password = getState().form.login.password.value;
+    const user = {
+      username: getState().form.login.username.value,
+      password: getState().form.login.password.value,
+    };
 
     return dispatch({
       types: [
@@ -20,7 +22,7 @@ export function loginUser() {
         LOGIN_USER_ERROR,
       ],
       payload: {
-        promise: login(username, password)
+        promise: login(user)
           .then((res) => {
             dispatch({
               type: FORM_RESET,
