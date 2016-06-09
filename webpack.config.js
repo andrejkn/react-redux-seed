d4b3a179147ad02917ba4ca6ce6dd0208b107612
Seth Davenport <SethDavenport@users.noreply.github.com>
2016-06-08 23:08:28 -0400

    Clean up webpack conf (#195)
    Also makes it consistent with changes that are happening in the karma-based starters.
    
    Connected to rangle/rangle-starter#127

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const proxy = require('./server/webpack-dev-proxy');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
-const SplitByPathPlugin = require('webpack-split-by-path');
+const plugins = require('./webpack/plugins');
+const postcss = require('./webpack/postcss');
 
 function getEntrySources(sources) {
   if (process.env.NODE_ENV !== 'production') {
@@ -13,70 +11,8 @@ function getEntrySources(sources) {
   return sources;
 }
 
-const basePlugins = [
-  new webpack.DefinePlugin({
-    __DEV__: process.env.NODE_ENV !== 'production',
-    __PRODUCTION__: process.env.NODE_ENV === 'production',
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  }),
-  new SplitByPathPlugin([
-    { name: 'vendor', path: [__dirname + '/node_modules/'] },
-  ]),
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-    inject: 'body',
-  }),
-];
-
-const devPlugins = [
-  new webpack.NoErrorsPlugin(),
-  new StyleLintPlugin({
-    configFile: './.stylelintrc',
-    files: ['src/**/*.css'],
-    failOnError: false,
-  }),
-];
-
-const prodPlugins = [
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      warnings: false,
-    },
-  }),
-  new webpack.NoErrorsPlugin(),
-];
-
-const plugins = basePlugins
-  .concat(process.env.NODE_ENV === 'production' ? prodPlugins : [])
-  .concat(process.env.NODE_ENV === 'development' ? devPlugins : []);
-
-const postcssBasePlugins = [
-  require('postcss-modules-local-by-default'),
-  require('postcss-import')({
-    addDependencyTo: webpack,
-  }),
-  require('postcss-cssnext')({
-    browsers: ['ie >= 8', 'last 2 versions'],
-  }),
-];
-const postcssDevPlugins = [];
-const postcssProdPlugins = [
-  require('cssnano')({
-    safe: true,
-    sourcemap: true,
-    autoprefixer: false,
-  }),
-];
-
-const postcssPlugins = postcssBasePlugins
-  .concat(process.env.NODE_ENV === 'production' ? postcssProdPlugins : [])
-  .concat(process.env.NODE_ENV === 'development' ? postcssDevPlugins : []);
-
 module.exports = {
-  entry: {
-    app: getEntrySources(['./src/index.js']),
-  },
+  entry: { app: getEntrySources(['./src/index.js']) },
 
   output: {
     path: path.join(__dirname, 'dist'),
@@ -108,7 +44,5 @@ module.exports = {
     ],
   },
 
-  postcss: function postcssInit() {
-    return postcssPlugins;
-  },
+  postcss: postcss,
 };
